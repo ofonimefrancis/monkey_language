ce6121563a8d1bae51e40d2bdd30c7c88ef145e9
Ofonime Usoro <ofonime.usoro@hellofresh.com>
2021-11-07 17:29:15 +0100

    implementing lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -2,24 +2,58 @@ package lexer
 
 import (
 	"testing"
-	token "github.com/ofonimefrancis/monkey_language/token"
+
+	"github.com/ofonimefrancis/monkey_language/token"
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},}`
+	input := `let age = 5;
+      let ten = 10;
+      let add = fn(x, y) {
+		x + y;
+      };
 
+    let result = add(age, ten);
+`
 
 	testCases := []struct{
 		expectedType token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "age"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
-		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "age"},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
